52337d90e92febb32f6632bf113814a6ae56c145
sstuarrt <95904642+sstuarrt@users.noreply.github.com>
2022-10-22 16:42:51 +0300

    Add files via upload

---
+++ lesson3.js
@@ -0,0 +1,44 @@
+function devide() {
+    console.log('-------------------------------------------------------------');
+}
+
+//1
+
+function show(products){
+    console.log(products);
+}
+
+var products = ['Pear', 'Apple', 'Milk', 'Bread', 'Cucumber', 'Banana'];
+
+const indexProductEnd = products.length - 1;
+
+show(products[indexProductEnd]);
+
+devide();
+
+//2
+
+var styles = ['Jazz', 'Blues'];
+
+const rock = 'Rock-n-Roll';
+
+styles.push(rock);
+show(styles);
+
+styles[1] = 'Classic';
+show(styles);
+
+show(styles.shift());
+
+styles.unshift('Rap', 'Reggy');
+show(styles);
+
+function find(arr, value) {
+    if(styles.values(value)) {
+        console.log(styles.indexOf(value))
+    } else {
+        console.log(-1)
+    }
+}
+
+find(styles, 'Reggy');
